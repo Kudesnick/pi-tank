e138bb985a2e0a474e281b3844753f8cb96e2ec4
Stulov Tikhon <kudesnick@inbox.ru>
2024-09-16 17:32:53 +0300

    fix syntax error

--- pwm-test.sh
+++ pwm-test.sh
@@ -8,18 +8,18 @@ PWM_PERIOD=50000000
 PWM_LEFT=0
 PWM_RIGHT=1
 
-pwm_duty {
+function pwm_duty {
     echo $(( ${PWM_PERIOD} / 100 * $2 )) > "${PWM}/pwm$1/duty_cycle"
 }
 
-pwm_export {
+function pwm_export {
     test -d "${PWM}/pwm$1" || echo $1 > "${PWM}/export"
     echo ${PWM_PERIOD} > "${PWM}/pwm$1/period"
     pwm_duty $1 $2
     echo 1 > "${PWM}/pwm$1/enable"
 }
 
-pwm_unexport {
+function pwm_unexport {
     echo 0 > "${PWM}/pwm$1/enable"
     echo $1 > "${PWM}/unexport"
 }
@@ -34,21 +34,21 @@ GPIO_RIGHT_BACK=23
 HI=1
 LOW=0
 
-gpio_set {
+function gpio_set {
     echo ${HI} > ${GPIO}/gpio$1/value
 }
 
-gpio_reset {
+function gpio_reset {
     echo ${LOW} > ${GPIO}/gpio$1/value
 }
 
-gpio_export {
+function gpio_export {
     test -d "${GPIO}/gpio$1" || echo $1 > "${GPIO}/export"
     echo out > ${GPIO}/gpio$1/direction
     gpio_reset $1
 }
 
-gpio_unexport {
+function gpio_unexport {
     echo $1 > "${GPIO}/unexport"
 }
 
@@ -58,36 +58,36 @@ H_LEFT="LEFT"
 H_RIGHT="RIGHT"
 
 function bridge_export {
-    pwm_export ${PWM_$1} 0
-    gpio_export ${GPIO_$1_FORWARD}
-    gpio_export ${GPIO_$1_BACK}
+    pwm_export $(eval echo "\$PWM_$1") 0
+    gpio_export $(eval echo "\$GPIO_$1_FORWARD")
+    gpio_export $(eval echo "\$GPIO_$1_BACK")
 }
 
 function bridge_unexport {
-    pwm_unexport ${PWM_$1}
-    gpio_unexport ${GPIO_$1_FORWARD}
-    gpio_unexport ${GPIO_$1_BACK}
+    pwm_unexport $(eval echo "\$PWM_$1")
+    gpio_unexport $(eval echo "\$GPIO_$1_FORWARD")
+    gpio_unexport $(eval echo "\$GPIO_$1_BACK")
 }
 
-bridge_stop {
-    pwm_duty ${PWM_$1} 0
-    gpio_reset ${GPIO_$1_FORWARD}
-    gpio_reset ${GPIO_$1_BACK}
-    pwm_duty ${PWM_$1} $2
+function bridge_stop {
+    pwm_duty $(eval echo "\$PWM_$1") 0
+    gpio_reset $(eval echo "\$GPIO_$1_FORWARD")
+    gpio_reset $(eval echo "\$GPIO_$1_BACK")
+    pwm_duty $(eval echo "\$PWM_$1") $2
 }
 
-bridge_forward {
-    pwm_duty ${PWM_$1} 0
-    gpio_set ${GPIO_$1_FORWARD}
-    gpio_reset ${GPIO_$1_BACK}
-    pwm_duty ${PWM_$1} $2
+function bridge_forward {
+    pwm_duty $(eval echo "\$PWM_$1") 0
+    gpio_set $(eval echo "\$GPIO_$1_FORWARD")
+    gpio_reset $(eval echo "\$GPIO_$1_BACK")
+    pwm_duty $(eval echo "\$PWM_$1") $2
 }
 
-bridge_back {
-    pwm_duty ${PWM_$1} 0
-    gpio_reset ${GPIO_$1_FORWARD}
-    gpio_set ${GPIO_$1_BACK}
-    pwm_duty ${PWM_$1} $2
+function bridge_back {
+    pwm_duty $(eval echo "\$PWM_$1") 0
+    gpio_reset $(eval echo "\$GPIO_$1_FORWARD")
+    gpio_set $(eval echo "\$GPIO_$1_BACK")
+    pwm_duty $(eval echo "\$PWM_$1") $2
 }
 
 # main
